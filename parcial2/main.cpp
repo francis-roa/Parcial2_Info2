5813eb9cacda79000178d0e7f0e42a4c8707fd9f
francis-roa <80040964+francis-roa@users.noreply.github.com>
2021-09-24 16:32:31 -0500

    algoritmo de reducción completado
    se terminó el algoritmo de reducción, se implementó el código de reducción como una función para extraer la información de los 3 colores y se consiguió escribir la información con el formato deseado en un archivo de texto.

--- parcial2/main.cpp
+++ parcial2/main.cpp
@@ -9,25 +9,45 @@
 
 using namespace std;
 
+void reduccion(QImage,char);
+void aumento(QImage);
+int agregar(char,QImage,int,int);
+ofstream outfile;
 
-int main(){
-    int cont;
-    int final = 0;
-    int sum = 0;
+int main()
+{
+    int redu=0;
+    string name = "../pruebas_parcial2/imagenes/col.png";
+    QImage im(name.c_str());
+    cin>>redu;
+    char r='r',g='g',b='b';
+    outfile.open("../pruebas_parcial2/datos.txt");
+    if(redu==2){
+        reduccion(im,r);
+        reduccion(im,g);
+        reduccion(im,b);
+    }
+    else{
+        aumento(im);
+    }
+    outfile.close();
+    return 0;
+}
+
+void reduccion(QImage im,char color){
+    int cont, final = 0, sum = 0, altoprom = im.height()/16;;
     vector <int> cred;
     vector <int> promred;
     vector <int> promred2;
-    string name = "../pruebas_parcial2/imagenes/col.png";
-    QImage im(name.c_str());
-    int altoprom = im.height()/16;
 
-
+    //se obtienen las nuevas filas
     for(int y=0;y<im.height();y++){
         cont = 0;
         final = 0;
         for(int x=0;x<im.width();x++){
             cont++;
-            cred.push_back(im.pixelColor(x,y).red());
+            cred.push_back(agregar(color,im,x,y));
+            //cred.push_back(im.pixelColor(x,y).red());
             if(cont==im.width()/16 || x==im.width()-1) {
                 cont = 0;
                 final ++;
@@ -47,22 +67,11 @@ int main(){
                 sum = 0;
                 }
             }
-
         }
     }
 
     //imprimir en consola las filas promediadas
-    /*int i=0;
-    int a;
-    cout << "ver vector" << endl;
-    cin>> a;
-    for (vector<int>::iterator  it = promred.begin() ; it != promred.end(); ++it){
-        i++;
-        cout << *it << " ";
-        if(i%16==0){cout<<endl;}
-
-    }*/
-
+    int i=0;
     cred.clear();
     int terminar=1;
     final=0;
@@ -70,6 +79,9 @@ int main(){
     int b = 0, baux=0;
     sum=0;
     int yep=0;
+
+    //se obtienen las nuevas columnas
+    //se rota la información hacia la izquierda
     while(terminar!=17){
 
         while(final!=16){
@@ -94,34 +106,51 @@ int main(){
         yep++;
         terminar++;
         b=0;
+        ko=0;
     }
-
-    /*int i=0;
+    i=0;
     int o=0,oaux=0;
     sum=0;
-    int a;
-    cout << "ver vector" << endl;
-    cin>> a;
+
+
+    //escribir el formato en un archivo de texto
+    //se "rota" la información
     for (int b=0;b<16*16;b++,sum=o*16+oaux){
         i++;
         o++;
-        cout << promred2[sum] << " ";
+        //escribir formato
+        if(b==0){
+            cout <<"{"<<promred2[sum] << ",";//inicio del formato
+            outfile<<"{"<<promred2[sum] << ",";
+        }
+        else if(b==16*16-1){
+            cout << promred2[sum] << "}";//fin del formato
+            outfile << promred2[sum] << "}";
+        }
+        else{
+            cout << promred2[sum] << " ,";//resto del formato
+            outfile <<promred2[sum] << ",";
+        }
         if(i%16==0){
             i=0;
             oaux++;
             o=0;
             cout<<endl;
-        }
-
-    }*/
-    for(unsigned long long i=0;i<promred2.size();i++){
-        cout<<promred2[i]<<" ";
-        if(i%16==0){
-            cout<<endl;
+            outfile << "\n";
         }
     }
     cout << promred2.size() << endl;
     cout << promred.size() << endl;
-    return 0;
+}
 
+void aumento(QImage){
+
+}
+
+int agregar(char col,QImage im,int x,int y){
+    int dato=0;
+    if(col=='r'){dato=im.pixelColor(x,y).red();}
+    else if(col=='g'){dato=im.pixelColor(x,y).green();}
+    else if(col=='b'){dato=im.pixelColor(x,y).blue();}
+    return dato;
 }
