c96a87228962048d62e455c1d9eb48a418b12fed
francis-roa <80040964+francis-roa@users.noreply.github.com>
2021-09-25 16:02:55 -0500

    se completan ambos algoritmos
    Luego de un número de pruebas, se completó la implementación de ambos algoritmos para los 3 colores RGB.

--- parcial2/main.cpp
+++ parcial2/main.cpp
@@ -9,34 +9,45 @@
 
 using namespace std;
 
-void reduccion(QImage,char);
-void aumentoc(QImage);
+void reduccionc(QImage,char);
+vector<int> reduccionf(QImage,vector<int>);
+void aumentoc(QImage,char);
 vector<int> aumentof(QImage, vector<int>);
+void igualc(QImage,char);
 int agregar(char,QImage,int,int);
+void escribir(vector<int>);
+vector<int> rotar(vector<int>);
 ofstream outfile;
 
 int main()
 {
-    int redu=0;
     string name = "../pruebas_parcial2/imagenes/ocho_ocho.jpg";
+    //string name = "../pruebas_parcial2/imagenes/col.png";
+    //string name = "../pruebas_parcial2/imagenes/colp.png";
     QImage im(name.c_str());
-    cin>>redu;
     char r='r',g='g',b='b';
     outfile.open("../pruebas_parcial2/datos.txt");
-    if(redu==2){
-        reduccion(im,r);
-        reduccion(im,g);
-        reduccion(im,b);
+    if(im.width()>16){
+        reduccionc(im,r);
+        reduccionc(im,g);
+        reduccionc(im,b);
+    }
+    else if(im.width()<16){
+        aumentoc(im,r);
+        aumentoc(im,g);
+        aumentoc(im,b);
     }
     else{
-        aumentoc(im);
+        igualc(im,r);
+        igualc(im,g);
+        igualc(im,b);
     }
     outfile.close();
     return 0;
 }
 
-void reduccion(QImage im,char color){
-    int cont, final = 0, sum = 0, altoprom = im.height()/16;;
+void reduccionc(QImage im,char color){
+    int cont, final = 0, sum = 0;
     vector <int> cred;
     vector <int> promred;
     vector <int> promred2;
@@ -48,7 +59,6 @@ void reduccion(QImage im,char color){
         for(int x=0;x<im.width();x++){
             cont++;
             cred.push_back(agregar(color,im,x,y));
-            //cred.push_back(im.pixelColor(x,y).red());
             if(cont==im.width()/16 || x==im.width()-1) {
                 cont = 0;
                 final ++;
@@ -71,18 +81,29 @@ void reduccion(QImage im,char color){
         }
     }
 
-    //imprimir en consola las filas promediadas
-    int i=0;
     cred.clear();
-    int terminar=1;
-    final=0;
-    int ko=0;
-    int b = 0, baux=0;
-    sum=0;
-    int yep=0;
+    if(im.height()<16)
+    {
+        promred2=aumentof(im,promred);
+    }
+    else if(im.height()>16){
+        promred2=reduccionf(im,promred);
+        promred.clear();
+        //se rota la información en sentido horario
+        promred2=rotar(promred2);
+    }
+    escribir(promred2);
+    cout << promred2.size() << endl;
+    cout << promred.size() << endl;
+}
+
+vector<int> reduccionf(QImage im, vector<int> promred)
+{
+    int terminar=1,final=0,ko=0,b = 0, baux=0,sum=0,yep=0,altoprom = im.height()/16;
+    vector<int> promred2;
 
     //se obtienen las nuevas columnas
-    //se rota la información hacia la izquierda
+    //se rota la información en sentido antihorario
     while(terminar!=17){
 
         while(final!=16){
@@ -92,7 +113,7 @@ void reduccion(QImage im,char color){
                 sum= sum + promred[b*16+yep];
                 b++;
                 baux++;
-                if(ko==1 && b==im.height()%16){break;}
+                if(ko==1 && baux==im.height()%16){break;}
             }
             if(final==15 && im.height()%16!=0 && ko!=1){ko=1;}
             else{final++;
@@ -109,70 +130,40 @@ void reduccion(QImage im,char color){
         b=0;
         ko=0;
     }
-    i=0;
-    int o=0,oaux=0;
-    sum=0;
-
-
-    //escribir el formato en un archivo de texto
-    //se "rota" la información
-    for (int b=0;b<16*16;b++,sum=o*16+oaux){
-        i++;
-        o++;
-        //escribir formato
-        if(b==0){
-            cout <<"{"<<promred2[sum] << ",";//inicio del formato
-            outfile<<"{"<<promred2[sum] << ",";
-        }
-        else if(b==16*16-1){
-            cout << promred2[sum] << "}";//fin del formato
-            outfile << promred2[sum] << "}";
-        }
-        else{
-            cout << promred2[sum] << " ,";//resto del formato
-            outfile <<promred2[sum] << ",";
-        }
-        if(i%16==0){
-            i=0;
-            oaux++;
-            o=0;
-            cout<<endl;
-            outfile << "\n";
-        }
-    }
-    cout << promred2.size() << endl;
-    cout << promred.size() << endl;
+    return promred2;
 }
 
-void aumentoc(QImage im){
+void aumentoc(QImage im, char color){
     int x1=im.width(),x2=16;
     vector <int> mod_data;
     for(int y=0;y<im.height();y++)
     {
         for(int x=0;x<x1;x++)
         {
-            mod_data.push_back(im.pixelColor(x,y).red());
+            mod_data.push_back(agregar(color, im, x, y));
             if(x1<=x+(x2-x1) && (x+x2-x1)<=x2)
             {
                 for(int clon=1;clon<x2/x1;clon++)
                 {
-                    mod_data.push_back(im.pixelColor(x,y).red());
+                    mod_data.push_back(agregar(color, im, x, y));
                 }
+            }
+            if(x+1==x1 && mod_data.size()%16!=0)
+            {
+                x=x-(x2-x1*2)+1;
             }
         }
     }
-    mod_data=aumentof(im,mod_data);
-    for(unsigned long long i=0;i<mod_data.size();i++)
+    if(im.height()<16)
     {
-        if(i%16==0 && i!=0)
-        {
-            cout<<endl<<mod_data[i]<<", ";
-        }
-        else
-        {
-            cout<<mod_data[i]<<", ";
-        }
+        mod_data=aumentof(im,mod_data);
     }
+    else if(im.height()>16)
+    {
+        mod_data=reduccionf(im,mod_data);
+        mod_data=rotar(mod_data);
+    }
+    escribir(mod_data);
     cout<<endl;
     cout<<mod_data.size();
 }
@@ -180,7 +171,16 @@ void aumentoc(QImage im){
 vector<int> aumentof(QImage im, vector<int> nuev_image)
 {
     int y1=im.height(), y2=16,y=0,x=0,itera=16,aumento=0;
-    vector<int>::iterator it=nuev_image.begin();
+    int iaux=0, copy;
+    vector<int>::iterator it;
+    vector<int> nuev;
+    nuev.reserve(256);
+    for(vector<int>::iterator i=nuev_image.begin(); i!=nuev_image.end();i++,iaux++)
+    {
+        copy=nuev_image[iaux];
+        nuev.push_back(copy);
+    }
+    it=nuev.begin();
     while(y < y2-y1)
     {
         if(y1<=y+(y2-y1) && (y+y2-y1)<=y2)
@@ -189,25 +189,102 @@ vector<int> aumentof(QImage im, vector<int> nuev_image)
             {
                 while(x<16)
                 {
-                    nuev_image.insert(it+itera,nuev_image[1*x+aumento]);
+                    //nuev_image.insert(it+itera,nuev_image[1*x+aumento]);
+                    //nuev[itera]=nuev_image[1*x+aumento];
+                    nuev.insert(it+itera,nuev[x+aumento]);
                     x=x+1;
                     itera=itera+1;
-                    it=nuev_image.begin();
+                    it=nuev.begin();
                 }
             }
         }
-        itera=itera+16;
+
         x=0;
-        y=y+1;
-        aumento=aumento+32;
+        if(y+1==y1 && nuev.size()/16<16)
+        {
+            y=y-(y2-y1*2)+1;
+            itera=16*y;
+        }
+        else{
+            y=y+1;
+            aumento=aumento+32;
+            itera=itera+16;
+        }
     }
-    return nuev_image;
+    return nuev;
 }
 
+//devuelve el valor R, G o B del pixel con coordenadas (x,y)
 int agregar(char col,QImage im,int x,int y){
     int dato=0;
     if(col=='r'){dato=im.pixelColor(x,y).red();}
     else if(col=='g'){dato=im.pixelColor(x,y).green();}
     else if(col=='b'){dato=im.pixelColor(x,y).blue();}
     return dato;
+}
+
+//se escriben los datos en un archivo de texto y se muestran en consola
+void escribir(vector<int> datos)
+{
+    for(int i=0; i<16*16;i++)
+    {
+        //escribir formato
+        if(i==0){
+            cout <<"{"<<datos[i] << ", ";//inicio del formato
+            outfile<<"{"<<datos[i] << ", ";
+        }
+        else if(i==16*16-1){
+            cout << datos[i] << "}";//fin del formato
+            outfile << datos[i] << "}\n";
+        }
+        else if(i%16==0 && i!=0){
+            cout<<endl<<datos[i]<<", ";//cambio de fila en el formato
+            outfile << "\n"<<datos[i]<<", ";
+        }
+        else{
+            cout << datos[i] << ", ";//resto del formato
+            outfile <<datos[i] << ", ";
+        }
+    }
+    outfile<<"\n";
+}
+
+void igualc(QImage im,char color)
+{
+    vector<int> data,data2;
+    for(int y=0;y<im.height();y++)
+    {
+        for(int x=0;x<im.width();x++)
+        {
+            data.push_back(agregar(color,im,x,y));
+        }
+    }
+    if(im.height()<16)
+    {
+        data=aumentof(im,data);
+
+    }
+    else if(im.height()>16)
+    {
+        data=reduccionf(im,data);
+        data=rotar(data);
+    }
+    escribir(data);
+}
+
+vector<int> rotar(vector<int> mod_data)
+{
+    int sum=0,o=0,oaux=0,i=0;
+    vector<int> mod_data2;
+    for (int b=0;b<16*16;b++,sum=o*16+oaux){
+        i++;
+        o++;
+        if(i%16==0){
+            i=0;
+            oaux++;
+            o=0;
+        }
+        mod_data2.push_back(mod_data[sum]);
+    }
+    return mod_data2;
 }
